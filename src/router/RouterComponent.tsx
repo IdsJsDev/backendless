c5306695dcdee61a19be24b17b9f3665222863f3
Dmitry <opengeekslabig@gmail.com>
2023-10-26 13:42:33 +0300

    fix: imports

--- src/router/RouterComponent.tsx
+++ src/router/RouterComponent.tsx
@@ -2,14 +2,17 @@ import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import React, { useMemo } from 'react';
 import RootLayout from '../Layouts/Root/Root.layout.tsx';
 import { useTabsContext } from '../context/tabs.context.ts';
+import { pagesList } from './pages.list.ts';
+
+const imports = pagesList.map((el) => ({ ...el, component: React.lazy(() => import(/* @vite-ignore */ el.path)) }));
 const RouterComponent: React.FC = () => {
   const { tabs } = useTabsContext();
 
   const array = useMemo(
     () =>
       tabs.map((tab, index) => {
-        const LazyComponent = React.lazy(() => import(/* @vite-ignore */ `/src/pages/${tab.path}.tsx`));
-        return <Route key={index} path={`/${tab.id}`} element={<LazyComponent />} />;
+        const LazyComponent = imports.find((el) => el.id === tab.id)?.component;
+        return LazyComponent ? <Route key={index} path={`/${tab.id}`} element={<LazyComponent />} /> : null;
       }),
     [tabs],
   );
---
+++ src/router/pages.list.ts
@@ -0,0 +1,14 @@
+export const pagesList = [
+  {
+    path: '/src/pages/DummyChart/DummyChart.tsx',
+    id: 'dummyChart',
+  },
+  {
+    path: '/src/pages/DummyList/DummyList.tsx',
+    id: 'dummyList',
+  },
+  {
+    path: '/src/pages/DummyTable/DummyTable.tsx',
+    id: 'dummyTable',
+  },
+];
