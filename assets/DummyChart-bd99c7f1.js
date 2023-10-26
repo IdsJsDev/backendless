e9cbd6b9d7cdcac57e83ab1db05ad5e083e6a66c
Dmitry <opengeekslabig@gmail.com>
2023-10-26 14:14:23 +0300

    Updates

---
+++ assets/DummyChart-bd99c7f1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-5411e259.js";const m=()=>r.jsx(r.Fragment,{children:"DummyChart"});export{m as default};
---
+++ assets/DummyList-90a4d681.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-5411e259.js";const r=()=>t.jsx(t.Fragment,{children:"DummyList"});export{r as default};
